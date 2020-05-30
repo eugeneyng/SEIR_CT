795b6b277677e98ea41910cee90ec75d8220c578
eugeneyng <ngyeugene@gmail.com>
2020-05-30 17:02:29 -0400

    ODE solution working

--- README.md
+++ README.md
@@ -3,15 +3,16 @@
 Modelling the control of SARS-CoV-2, COVID-19, the novel coronavirus, "rona."
 
 # Assumptions
-* The natural rate is equal to the natural death rate
-* Age is not taken into account x(t) != x(t,a)
+* Age is not taken into account [i.e. x(t) != x(t,a)]
 * Hospitalization/ICU rates are not taken into account
 * The average incubation period is a random variable with exponential distribution with parameter a (the average incubation period is a^-1)
 
 # Sources
 
-* https://www.cdc.gov/covid-data-tracker/index.html
-* https://portal.ct.gov/Coronavirus
-* https://www.worldometers.info/coronavirus/country/us/
+* [CDC COVID Tracker](https://www.cdc.gov/covid-data-tracker/index.html)
+* [CT COVID Portal](https://portal.ct.gov/Coronavirus)
 * [Birth Rate](https://www.cdc.gov/nchs/data/nvsr/nvsr67/nvsr67_08-508.pdf)
 * [Death Rate](https://www.cdc.gov/nchs/data/nvsr/nvsr68/nvsr68_09-508.pdf)
+* [Life Expectancy](https://www.cdc.gov/nchs/data-visualization/life-expectancy/index.html)
+* [Recovery Period](https://www.who.int/docs/default-source/coronaviruse/who-china-joint-mission-on-covid-19-final-report.pdf)
+* [World COVID Tracker](https://www.worldometers.info/coronavirus/country/us/)
--- main.py
+++ main.py
@@ -1,35 +1,89 @@
-# EUGENE NG
-
+import matplotlib.pyplot as plt
+import numpy
 import scipy
+import scipy.integrate
 
-# Global Constants
-N = 3.57e6 # Total Population of CT [persons]
-a = 0.2 # Average Incubation Period [days^-1]
-beta = 1 # Rate of Exposure
-sigma = 1 # Rate at which an exposed person becomes infective [days^-1]
-gamma = 0.001 # Rate of Recovery [days^-1] [0.001, 0.05]
-cfr = 0.025 # Case Fatality Rate [2-3%]
-delta = 9.8 # Natural Birth Rate (unrelated to disease)
-mu = 8.7 # Natural Death Rate (unrelated to disease)
-R0 = (a/(mu+a))*(beta/(mu+gamma)) # Basic Reproduction Number
-t = 365 # How long the model will run
-dt = 1 # Step Size
+# GLOBAL CONSTANTS
+delta = 2.703e-5 # Natural Birth Rate (unrelated to disease)
+mu = 2.403e-5 # Natural Death Rate (unrelated to disease)    beta = 1 # Rate of Exposure
+sigma = 0.2 # Average Incubation Period [days^-1]
 
 def main():
-    t = 41303 # Total Number of Cases [persons]
-    d = 3769  # Number of Deaths [persons]
-    s = t-d-r # Number of Susceptible [persons]
-    e = 0     # Number of Exposed [persons]
-    i = 30912 # Number of Active Infected [persons]
-    r = t-d-i # Number of Recovered [persons]
+
+    # INITIAL CONDITIONS
+    beta0 = 0.175 # Rate of Exposure
+    N0 = 3.57e6 # Total Population of CT [persons]
+    gamma0 = 0.07 # Rate of Removal [days^-1]
+    cfr0 = 0.025 # Case Fatality Rate [2-3%]
+    R0 = (beta0*sigma)/((mu+gamma0)*(mu+sigma)) # Basic Reproduction Number
+
+    c0 = 41762 # Total Number of Cases [persons]
+    d0 = 3868  # Number of Deaths [persons]
+    i0 = 30383/N0 # Active Infected []
+    r0 = (c0-d0-i0)/N0 # Recovered []
+    e0 = (R0*i0)/N0  # Exposed []
+    s0 = (N0-c0-d0-r0)/N0 # Susceptible []
+
+    # ODE VARIABLES
+    tf = 365
+    dt = 1
+    tspan = [0, tf]
+    teval = numpy.arange(0, tf, dt)
+
+    # INIT
+    init = [N0, c0, d0, s0, e0, i0, r0, beta0, gamma0, cfr0]
+
+    # SOLVE ODE
+    solution = scipy.integrate.solve_ivp(rhs, tspan, init, t_eval=teval)
+    solution = numpy.c_[teval, numpy.transpose(solution['y'])]
+    numpy.savetxt('solution.csv', solution, header='Time, Population, Cases, Dead, Susceptible, Exposed, Infected, Removed, beta, gamma, CFR')
+
+    fig = plt.figure()
+    fig.add_subplot(1,2,1)
+    plt.plot(teval, solution[:,4], label='Susceptible')
+    plt.plot(teval, solution[:,5], label='Exposed')
+    plt.plot(teval, solution[:,6], label='Infected')
+    plt.plot(teval, solution[:,7], label='Removed')
+    plt.legend()
+
+    fig.add_subplot(1,2,2)
+    # plt.plot(teval, solution[:,1], label="Population")
+    plt.plot(teval, solution[:,2], label="Cases")
+    plt.plot(teval, solution[:,3], label="Dead")
+    plt.legend()
+
+    plt.show()
 
 def rhs(dt, init):
 
+    # Unpack Arguments
+    N = init[0]
+    c = init[1]
+    d = init[2]
+    s = init[3]
+    e = init[4]
+    i = init[5]
+    r = init[6]
+    beta = init[7]
+    gamma = init[8]
+    cfr = init[9]
 
-def dynamics(args):
+    # Solve dynamics
+    sdot = delta - (beta*s*i) - (mu*s)
+    edot = (beta*s*i) - (mu*e) - (sigma*e)
+    idot = (sigma*e) - (gamma*i) - (mu*i)
+    rdot = (gamma*i) - (mu*r)
+    betadot = 0
+    sigmadot = 0
+    gammadot = 0
+    cfrdot = 0
 
-    # Unpack args
-    s = args[0]
-    e = args[1]
-    i = args[2]
-    r = args[3]
+    # Track variables
+    Ndot = (delta*N) - (mu*N) - (cfr*rdot*N)
+    ddot = cfr*rdot*N
+    cdot = edot*N
+
+    return [Ndot, cdot, ddot, sdot, edot, idot, rdot, betadot, gammadot, cfrdot]
+
+if __name__ == "__main__":
+    main()
