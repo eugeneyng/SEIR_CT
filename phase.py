87fd99a338c62d3099182933554c2d9938c0a561
eugeneyng <ngyeugene@gmail.com>
2020-07-01 00:11:30 -0400

    minor changes

--- phase.py
+++ phase.py
@@ -10,22 +10,25 @@ import sympy
 beta0 = 0.175
 gamma0 = 0.07
 
-def dxdt(x, t=0):
+def dRIdt(x, t=0):
     return numpy.array([ gamma0 * x[1],
                         beta0 * numpy.exp( (-beta0/gamma0) * x[0] ) - (gamma0 * x[1]) ])
 
-def phase():
+def phaseRI():
     # see https://scipy-cookbook.readthedocs.io/items/LoktaVolterraTutorial.html
-    r = numpy.linspace(0, 1, 20)
-    i = numpy.linspace(0, 1, 20)
+    r = numpy.linspace(0, 1, 50)
+    i = numpy.linspace(0, 1, 50)
     R, I = numpy.meshgrid(r,i)
-    dR, dI = dxdt([R,I])
+    dR, dI = dRIdt([R,I])
     M = (numpy.hypot(dR, dI))
     M [ M == 0 ] = 1.
     dR /= M
     dI /= M
-    Q = plt.quiver(R, I, dR, dI, M, pivot='mid')
+    # plt.quiver(R, I, dR, dI, M, pivot='mid')
+    plt.streamplot(R, I, dR, dI)
+    plt.xlabel("Recovered")
+    plt.ylabel("Infected")
     plt.show()
 
 if __name__ == "__main__":
-    phase()
+    phaseRI()
