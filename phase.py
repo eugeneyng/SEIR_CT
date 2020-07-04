bb3ecb9ca6c6087ab0f90565f19ae2136832f2b0
ngyeu <ngyeugene@gmail.com>
2020-07-04 00:09:44 -0400

    adding SI phase portrait

--- phase.py
+++ phase.py
@@ -14,6 +14,14 @@ def dRIdt(x, t=0):
     return numpy.array([ gamma0 * x[1],
                         beta0 * numpy.exp( (-beta0/gamma0) * x[0] ) - (gamma0 * x[1]) ])
 
+def dSIdt(x, t=0):
+    return numpy.array([ -beta0 * x[0] * x[1],
+                        beta0 * x[0] * x[1] - gamma0 * x[1] ])
+
+def dSRdt(x, t=0):
+    # return numpy.array([])
+    pass
+
 def phaseRI():
     # see https://scipy-cookbook.readthedocs.io/items/LoktaVolterraTutorial.html
     r = numpy.linspace(0, 1, 50)
@@ -30,5 +38,21 @@ def phaseRI():
     plt.ylabel("Infected")
     plt.show()
 
+def phaseSI():
+    s = numpy.linspace(0, 1, 50)
+    i = numpy.linspace(0, 1, 50)
+    S, I = numpy.meshgrid(s,i)
+    dS, dI = dSIdt([S,I])
+    M = (numpy.hypot(dS, dI))
+    M [ M == 0 ] = 1.
+    dS /= M
+    dI /= M
+    # plt.quiver(R, I, dR, dI, M, pivot='mid')
+    plt.streamplot(S, I, dS, dI)
+    plt.xlabel("Susceptible")
+    plt.ylabel("Infected")
+    plt.show()
+
 if __name__ == "__main__":
-    phaseRI()
+    # phaseRI()
+    phaseSI()
